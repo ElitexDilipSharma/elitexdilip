197d97517033c16f0f3e78ee7285005c6cd482d3
Dilip Sharma <146672867+dilip9158@users.noreply.github.com>
2024-05-15 11:44:56 +0530

    calculator

--- script.js
+++ script.js
@@ -1,75 +1,28 @@
-const lengthSlider = document.querySelector(".pass-length input"),
-  options = document.querySelectorAll(".option input"),
-  copyIcon = document.querySelector(".input-box span"),
-  passwordInput = document.querySelector(".input-box input"),
-  passIndicator = document.querySelector(".pass-indicator"),
-  generateBtn = document.querySelector(".generate-btn");
-
-const characters = {
-  lowercase: "abcdefghijklmnopqrstuvwxyz",
-  uppercase: "ABCDEFGHIJKLMNOPQRSTUVWXYZ",
-  numbers: "0123456789",
-  symbols: "^!$%&|[](){}:;.,*+-#@<>~"
-};
-
-const generatePassword = () => {
-  let staticPassword = "",
-    randomPassword = "",
-    excludeDuplicate = false,
-    passLength = lengthSlider.value;
-
-  options.forEach((option) => {
-    if (option.checked) {
-      if (option.id !== "exc-duplicate" && option.id !== "spaces") {
-        staticPassword += characters[option.id];
-      } else if (option.id === "spaces") {
-        staticPassword += `  ${staticPassword}  `;
-      } else {
-        excludeDuplicate = true;
-      }
-    }
-  });
-
-  for (let i = 0; i < passLength; i++) {
-    let randomChar =
-      staticPassword[Math.floor(Math.random() * staticPassword.length)];
-    if (excludeDuplicate) {
-      !randomPassword.includes(randomChar) || randomChar == " "
-        ? (randomPassword += randomChar)
-        : i--;
-    } else {
-      randomPassword += randomChar;
-    }
-  }
-  passwordInput.value = randomPassword;
-};
-
-const updatePassIndicator = () => {
-  passIndicator.id =
-    lengthSlider.value <= 8
-      ? "weak"
-      : lengthSlider.value <= 16
-      ? "medium"
-      : "strong";
-};
-
-const updateSlider = () => {
-  document.querySelector(".pass-length span").innerText = lengthSlider.value;
-  generatePassword();
-  updatePassIndicator();
-};
-updateSlider();
-
-const copyPassword = () => {
-  navigator.clipboard.writeText(passwordInput.value);
-  copyIcon.innerText = "check";
-  copyIcon.style.color = "#4285F4";
-  setTimeout(() => {
-    copyIcon.innerText = "copy_all";
-    copyIcon.style.color = "#707070";
-  }, 1500);
-};
-
-copyIcon.addEventListener("click", copyPassword);
-lengthSlider.addEventListener("input", updateSlider);
-generateBtn.addEventListener("click", generatePassword);
+const display = document.querySelector('#display');  
+ const buttons = document.querySelectorAll('button');  
+ buttons.forEach((item) => {  
+   item.onclick = () => {  
+     if (item.id == 'clear') {  
+       display.innerText = '';  
+     } else if (item.id == 'backspace') {  
+       let string = display.innerText.toString();  
+       display.innerText = string.substr(0, string.length - 1);  
+     }else if (display.innerText != '' && item.id == 'equal') {  
+       display.innerText = eval(display.innerText);  
+     }else if (display.innerText == '' && item.id == 'equal') {  
+       display.innerText = 'Empty!';  
+       setTimeout(() => (display.innerText = ''), 2000);  
+     }else {  
+       display.innerText += item.id;  
+     }  
+   }  
+ })  
+ const themeToggleBtn = document.querySelector('.theme-toggler');  
+ const calculator = document.querySelector('.dark');  
+ const toggleIcon = document.querySelector('.toggler-icon');  
+ let isDark = true;  
+ themeToggleBtn.onclick = () => {  
+   calculator.classList.toggle('dark');  
+   themeToggleBtn.classList.toggle('active');  
+   isDark = !isDark;  
+ }  
